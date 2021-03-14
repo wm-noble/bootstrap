af7a5e87a77754b644cc48ec2127162f765ae27f
Will <author@will.party>
2021-03-14 17:15:29 -0600

    Use a stylized subset of Bootstrap

--- js/index.umd.js
+++ js/index.umd.js
@@ -7,26 +7,26 @@
 
 import Alert from './src/alert'
 import Button from './src/button'
-import Carousel from './src/carousel'
-import Collapse from './src/collapse'
+//import Carousel from './src/carousel'
+//import Collapse from './src/collapse'
 import Dropdown from './src/dropdown'
 import Modal from './src/modal'
-import Popover from './src/popover'
-import ScrollSpy from './src/scrollspy'
-import Tab from './src/tab'
-import Toast from './src/toast'
+//import Popover from './src/popover'
+//import ScrollSpy from './src/scrollspy'
+//import Tab from './src/tab'
+//import Toast from './src/toast'
 import Tooltip from './src/tooltip'
 
 export default {
   Alert,
   Button,
-  Carousel,
-  Collapse,
+  //Carousel,
+  //Collapse,
   Dropdown,
   Modal,
-  Popover,
-  ScrollSpy,
-  Tab,
-  Toast,
+  //Popover,
+  //ScrollSpy,
+  //Tab,
+  //Toast,
   Tooltip
 }
